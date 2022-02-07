bc7c5e0fc9da18efca5f0736273d76094145bca2
wrangelvid <wrangelvid@gmail.com>
2022-02-07 21:11:01 +0000

    added simple publisher

---
+++ simple_publisher.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Float32
+import random
+
+def simple_publisher():
+  pub = rospy.Publisher('my_random_float', Float32, queue_size = 10)
+  rospy.init_node('simple_publisher')
+  rate = rospy.Rate(20)
+  while not rospy.is_shutdown():
+    num = random.uniform(0,10)
+    rospy.loginfo(num)
+    pub.publish(num)
+    rate.sleep()
+    
+if __name__ == '__main__':
+  try:
+    simple_publisher()
+  except rospy.ROSInterruptException:
+    pass
