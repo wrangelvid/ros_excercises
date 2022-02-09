99f858eb133a04d101c82434ce0e9bf81957fcd8
wrangelvid <wrangelvid@gmail.com>
2022-02-09 21:41:50 +0000

    added openSpace

--- open_space_publisher.py
+++ open_space_publisher.py
@@ -2,10 +2,12 @@
 import rospy
 from std_msgs.msg import Float32
 from sensor_msgs.msg import LaserScan
+from ros_exercises.msg import OpenSpace
 import math
 
-pub_distance = rospy.Publisher('distance', Float32, queue_size = 10)
-pub_angle = rospy.Publisher('angle', Float32, queue_size = 10)
+pub_distance = rospy.Publisher('open_space/distance', Float32, queue_size = 10)
+pub_angle = rospy.Publisher('open_space/angle', Float32, queue_size = 10)
+pub = rospy.Publisher('open_space', OpenSpace, queue_size = 10)
 
 def get_longest_range(msg):
   scan = msg
@@ -16,6 +18,14 @@ def get_longest_range(msg):
   rospy.loginfo(Float32(longest_range))
   pub_distance.publish(Float32(longest_range))
   pub_angle.publish(Float32(angle))
+
+  os_data = OpenSpace()
+  os_data.angle = angle
+  os_data.distance = longest_range
+  pub.publish(os_data)
+
+
+  
 
   
 def listener():
